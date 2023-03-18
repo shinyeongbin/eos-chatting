353313f9dafa6e27de1c68acc40829a7bfaf25d1
youngbin03 <sinyoungbin0128@gmail.com>
2023-03-18 19:54:45 +0900

    loginpage

---
+++ eos_chatting/lib/screens/main_screen.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import '../config/palette.dart';
+
+class LoginSignUpScreen extends StatefulWidget {
+  const LoginSignUpScreen({Key? key}) : super(key: key);
+  @override
+  State<LoginSignUpScreen> createState() => _LoginSignUpScreenState();
+}
+
+class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
+  bool isSigupScreen = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palette.backgroundColor,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/background.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Container(
+                  padding: EdgeInsets.only(top: 90, left: 20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: "Welcome",
+                            style: TextStyle(
+                                letterSpacing: 1.0,
+                                fontSize: 25,
+                                color: Colors.white),
+                            children: [
+                              TextSpan(
+                                text: " to EOS chat",
+                                style: TextStyle(
+                                  letterSpacing: 1.0,
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          "Signup to continue",
+                          style: TextStyle(
+                              letterSpacing: 1.0, color: Colors.white),
+                        ),
+                      ])),
+            ),
+          ),
+          Positioned(
+            top: 180,
+            child: Container(
+              height: 280.0,
+              width: MediaQuery.of(context).size.width - 40,
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 15,
+                    spreadRadius: 5,
+                  )
+                ],
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isSigupScreen = false;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text(
+                              "LOGIN",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: isSigupScreen
+                                    ? Palette.textColor1
+                                    : Palette.activeColor,
+                              ),
+                            ),
+                            if (!isSigupScreen)
+                              Container(
+                                margin: EdgeInsets.only(top: 3),
+                                height: 2,
+                                width: 55,
+                                color: Colors.green,
+                              ),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isSigupScreen = true;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text(
+                              "SIGNUP",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: isSigupScreen
+                                    ? Palette.activeColor
+                                    : Palette.textColor1,
+                              ),
+                            ),
+                            if (isSigupScreen)
+                              Container(
+                                margin: EdgeInsets.only(top: 3),
+                                height: 2,
+                                width: 55,
+                                color: Colors.green,
+                              ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    child: Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.account_circle,
+                                color: Palette.iconColor,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Palette.textColor1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(35)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Palette.textColor1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(35)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
